bcf39acc09fac606b19f4bd016e06491b5af0075
T. Mohan Siva <mohansivathota@gmail.com>
2026-04-22 10:11:39 +0530

    cta & intro video remove

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import "@/App.css";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HeroIntro from "./components/HeroIntro";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import ProcessSection from "./components/ProcessSection";
@@ -11,37 +9,29 @@ import HowItWorks from "./components/HowItWorks";
 import AudioPortfolio from "./components/AudioPortfolio";
 import ValueProps from "./components/ValueProps";
 import Testimonials from "./components/Testimonials";
-import Pricing from "./components/Pricing";
 import FAQ from "./components/FAQ";
 import FinalCTA from "./components/FinalCTA";
 import Footer from "./components/Footer";
 import FloatingWhatsApp from "./components/FloatingWhatsApp";
 
 const Landing = () => {
-  const [isEntryDone, setIsEntryDone] = useState(false);
-
   return (
     <div className="relative min-h-screen bg-[#080202] text-white grain">
-      {!isEntryDone ? (
-        <HeroIntro onComplete={() => setIsEntryDone(true)} />
-      ) : (
-        <VideoBackdrop enabled>
-          <main>
-            <Hero />
-            <ProcessSection />
-            <About />
-            <MelodyFinder />
-            <HowItWorks />
-            <AudioPortfolio />
-            <ValueProps />
-            <Testimonials />
-            <Pricing />
-            <FAQ />
-            <FinalCTA />
-          </main>
-          <Footer />
-        </VideoBackdrop>
-      )}
+      <VideoBackdrop enabled>
+        <main>
+          <Hero />
+          <ProcessSection />
+          <About />
+          <MelodyFinder />
+          <HowItWorks />
+          <AudioPortfolio />
+          <ValueProps />
+          <Testimonials />
+          <FAQ />
+          <FinalCTA />
+        </main>
+        <Footer />
+      </VideoBackdrop>
       <FloatingWhatsApp />
     </div>
   );
@@ -49,13 +39,11 @@ const Landing = () => {
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
